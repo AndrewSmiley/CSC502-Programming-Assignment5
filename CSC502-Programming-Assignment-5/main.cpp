1b68b499b752ee5ee5c95982d7b18799c82d689b
Andrew Smiley <andrew.smiley@live.com>
2016-04-26 19:28:13 -0400

    Initial Commit

---
+++ CSC502-Programming-Assignment-5/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CSC502-Programming-Assignment-5
+//
+//  Created by Andrew Smiley on 4/26/16.
+//  Copyright © 2016 Andrew Smiley. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
